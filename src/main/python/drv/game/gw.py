e4394822877386182c1972ed894a0026fb31cdc0
Peleg Michaeli <peleg@palgo-at.com>
2014-08-22 16:31:05 +0300

    Games workshop module.

---
+++ src/main/python/drv/game/gw.py
@@ -0,0 +1,25 @@
+"""
+.. gw.py
+
+Games Workshop is a British game production and retailing company. This module
+provides some of its dice mechanics.
+"""
+
+## Framework
+import drv.game.base
+
+## Sugar
+dk = drv.game.base.dk
+
+
+## This dice is used, according to Wikipedia, in Necromunda and Mordheim
+D66 = dk(6) * 10 + dk(6)
+
+
+## This is introduced, according to Wikipedia, in Blood Bowl
+def db(x):
+    """ Return a random variable which rolls a "block die" *x* times. """
+    ## This is usually marked Xdb, where x is one of 3, 2, 1 (usually omitted),
+    ## -2 or -3.
+    raise NotImplementedError
+
