4adaf26ce330d1b973884f4718d26cee523ade2b
Peleg Michaeli <peleg@palgo-at.com>
2014-08-22 16:30:53 +0300

    Introducing notations to fudge dice.

--- src/main/python/drv/game/fudge.py
+++ src/main/python/drv/game/fudge.py
@@ -11,14 +11,15 @@ POOL = drv.core.RandomVariablePool
 dk = drv.game.base.dk
 
 
+## Fudge dice notation is according to Wikipedia
 fudge_die = dk(3) - 2
-fudge_die.name = 'fudge'
+fudge_die.name = 'dF'
 
 
 def test(skill, target):
     """ Return a random variable which rolls 4 fudge die, adds it to *skill*,
     and checks whether it is at least *target*. """
-    fudge_sum = POOL(*([fudge_die] * 4)).sum('fudge_sum')
+    fudge_sum = POOL(*([fudge_die] * 4)).sum('4dF')
     tst = (fudge_sum + skill) >= target
     tst.mask = {1: 'Success', 0: 'Failure'}
     return tst
