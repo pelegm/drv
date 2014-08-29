fe07d6aa58c65b333d8465a97992635e50f5ae79
Peleg Michaeli <peleg@palgo-at.com>
2014-08-29 12:50:03 +0300

    More 3d6 examples.

--- tests/3d6.py
+++ tests/3d6.py
@@ -1,23 +1,42 @@
 #: Rolling three 6-sided dice, checking the results.
 #####
-#: Mean and standard deviation of 3d6
-#123456789#123456789#123456789#123456789#123456789#123456789#123456789#12345678
-## The following code calculates and prints basic statistical results about the
-## random variable representing 3d6.
+#: Basic properties of 3d6
 import drv.game.base
+
+## We define a random variable which represents
+## 3d6
 dice = drv.game.base.ndk(3, 6)
+
+## The following prints its minumum and maximum
+## values
+print "Min:", dice.min
+print "Max:", dice.max
+
+## The following  calculates and prints
+## basic statistical results
 print "Mean:", dice.mean
 print "STD:", dice.std
 print "Variance:", dice.variance
-#####
-#: Minimum and maximum of 3d6
-import drv.game.base
-dice = drv.game.base.ndk(3, 6)
-print "Min:", dice.min
-print "Max:", dice.max
+print "Median:", dice.median
 #####
 #: Probability of getting at least 13
 import drv.game.base
+
+## Again, define the random variable
 dice = drv.game.base.ndk(3, 6)
+
+## Define a "hit" function
 hit = lambda d: d >= 13
+
+## Calculate and print the probability
 print "Probability: {:.2%}".format(dice.pr(hit))
+#####
+#: Roll 3d6 a couple of times
+import drv.core
+drv.core.seed(2014)
+import drv.game.base
+
+## Define the random variable and roll
+dice = drv.game.base.ndk(3, 6)
+[dice.roll() for _ in xrange(6)]
+
