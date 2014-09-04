c9cad6146129c3fb9e570f86980746f40864f2cf
Peleg Michaeli <peleg@palgo-at.com>
2014-09-04 17:40:24 +0300

    Plotting tools are more general.

--- drv/plot_tools.py
+++ drv/plot_tools.py
@@ -4,10 +4,17 @@
 This module provides plotting facilities for the discrete random variables.
 """
 
+## Tools
 import functools as fn
 import textwrap as tw
+
+## Plot library
 import matplotlib.pyplot as plt
 
+
+#######################
+## ----- Utils ----- ##
+#######################
 
 def _y_of_path(xs, ys, x, n=100):
     """ Find the ``y`` coordinate of *line* in a given *x*. Do it as to
@@ -23,6 +30,10 @@ def _y_of_path(xs, ys, x, n=100):
     return prev_b + slope * (x - prev_a)
 
 
+########################################
+## ----- General Plot Functions ----- ##
+########################################
+
 def _plot_axes(xlabel=None, ylabel=None, title=None, figsize=None, dpi=None):
     fig = plt.figure(figsize=figsize, dpi=dpi)
     ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
@@ -36,62 +47,6 @@ def _plot_axes(xlabel=None, ylabel=None, title=None, figsize=None, dpi=None):
     return fig, ax
 
 
-def _plot_curve_mean(x, y, mean):
-    mean_y = _y_of_path(x, y, mean)
-    if mean_y is None:
-        return
-
-    xy_mean_text = mean, max(y) * 1.2
-    plt.annotate("Mean", xy=(mean, mean_y), xytext=xy_mean_text,
-                 arrowprops=dict(arrowstyle='->', facecolor='green'),
-                 color='green')
-
-
-def _plot_curve_std(x, y, mean, std):
-    left_x = mean - std
-    left_y = _y_of_path(x, y, left_x)
-    if left_y is None:
-        return
-    plt.plot([left_x, left_x], [0, left_y], color='red')
-    right_x = mean + std
-    right_y = _y_of_path(x, y, right_x)
-    if right_y is None:
-        return
-    plt.plot([right_x, right_x], [0, right_y], color='red')
-
-    mid_y = min(left_y, right_y) * 0.5
-    xy_std_text = mean, mid_y
-    plt.annotate("STD", xy=(right_x, mid_y), xytext=xy_std_text,
-                 arrowprops=dict(arrowstyle='->'), color='purple')
-
-
-def _plot_curve(x, y, mean=None, std=None, xlabel=None, ylabel=None, mask=None,
-                title=None, figsize=None, dpi=None):
-    ## Set figure and axes
-    fig, ax = _plot_axes(xlabel=xlabel, ylabel=ylabel, title=title,
-                         figsize=figsize, dpi=dpi)
-
-    ## Plot the curve itself
-    plt.plot(x, y)
-
-    ## Point to mean
-    if mean is not None:
-        _plot_curve_mean(x, y, mean)
-
-    ## Set x and y limits
-    ax.set_xlim(min(x), max(x))
-    ymin, ymax = ax.get_ylim()
-    if mean:
-        ymax *= 1.2 * 1.1
-    ax.set_ylim(ymin, ymax)
-
-    ## Show standard deviation
-    if mean and std:
-        _plot_curve_std(x, y, mean, std)
-
-    return fig
-
-
 def _plot_xkcd(plot_func, *args, **kwargs):
     """ Plot with *plot_func*, *args* and **kwargs*, but in xkcd style. """
     with plt.xkcd():
@@ -99,15 +54,15 @@ def _plot_xkcd(plot_func, *args, **kwargs):
     return fig
 
 
-def _plot_pmf(plot_func, drv, filename=None, mean=False, std=False,
-              plot_kwargs=None):
-    plot_kwargs = plot_kwargs or {}
-    x, y = drv.pmf_graph()
+def _plot_graph(plot_func, drv, method, mean=False, std=False, filename=None,
+                xkcd=False, **kwargs):
+    x, y = drv.graph(method)
 
     xlabel = 'RESULT'
     ylabel = 'PROBABILITY'
-    title = drv.name
-    kwargs = dict(xlabel=xlabel, ylabel=ylabel, title=title)
+    title = "{name} ({meth})".format(name=drv.name,
+                                     meth=method.im_func.func_name)
+    kwargs.update(xlabel=xlabel, ylabel=ylabel, title=title)
 
     ## Mask
     kwargs['mask'] = getattr(drv, 'mask', None)
@@ -117,7 +72,9 @@ def _plot_pmf(plot_func, drv, filename=None, mean=False, std=False,
     if std:
         kwargs['std'] = drv.std
 
-    kwargs.update(plot_kwargs)
+    ## Make figure
+    if xkcd:
+        plot_func = fn.partial(_plot_xkcd, plot_func)
     fig = plot_func(x, y, **kwargs)
 
     ## Show
@@ -129,28 +86,90 @@ def _plot_pmf(plot_func, drv, filename=None, mean=False, std=False,
         fig.savefig(filename, dpi=fig.dpi)
 
 
-def plot_pmf_curve(drv, filename=None, mean=False, std=False, figsize=None,
-                   dpi=None, xkcd=False):
-    """ Plot the probability mass function of the random variable *rv*; if
-    *filename* is given, save the figure, otherwise show it. If *xkcd*, plot it
-    in ``xkcd`` style. """
-    plotter = _plot_curve
-    if xkcd:
-        plotter = fn.partial(_plot_xkcd, plotter)
-    plot_kwargs = dict(figsize=figsize, dpi=dpi)
-    _plot_pmf(plotter, drv, filename=filename, mean=mean, std=std,
-              plot_kwargs=plot_kwargs)
-
-
 def _format_tick(mask, tick):
     return "\n".join(tw.wrap(mask.get(tick, ''), width=10))
 
 
-def _plot_bars(x, y, xlabel=None, ylabel=None, mask=None, title=None,
-               figsize=None, dpi=None):
+######################################
+## ----- Curve Plot Functions ----- ##
+######################################
+
+def _plot_curve_mean(x, y, mean):
+    mean_y = _y_of_path(x, y, mean)
+    if mean_y is None:
+        return
+
+    dy = max(y) - min(y)
+    xy_mean_text = mean, max(y) + dy * 0.2
+    plt.annotate("Mean", xy=(mean, mean_y), xytext=xy_mean_text,
+                 arrowprops=dict(arrowstyle='->', facecolor='green'),
+                 color='green')
+
+
+def _plot_curve_std(x, y, mean, std):
+    left_x = mean - std
+    left_y = _y_of_path(x, y, left_x)
+    if left_y is None:
+        return
+    ymin = min(y)
+    plt.plot([left_x, left_x], [ymin, left_y], color='red')
+    right_x = mean + std
+    right_y = _y_of_path(x, y, right_x)
+    if right_y is None:
+        return
+    plt.plot([right_x, right_x], [ymin, right_y], color='red')
+
+    mid_y = min(left_y - ymin, right_y - ymin) * 0.5 + ymin
+    xy_std_text = mean, mid_y
+    plt.annotate("STD", xy=(right_x, mid_y), xytext=xy_std_text,
+                 arrowprops=dict(arrowstyle='->'), color='purple')
+
+
+def _plot_curve(x, y, mean=None, std=None, mask=None, **kwargs):
     ## Set figure and axes
-    fig, ax = _plot_axes(xlabel=xlabel, ylabel=ylabel, title=title,
-                         figsize=figsize, dpi=dpi)
+    fig, ax = _plot_axes(**kwargs)
+
+    ## Plot the curve itself
+    plt.plot(x, y)
+
+    ## Point to mean
+    if mean is not None:
+        _plot_curve_mean(x, y, mean)
+
+    ## Set x and y limits
+    ax.set_xlim(min(x), max(x))
+    dy = max(y) - min(y)
+    ymin = min(y)
+    if mean:
+        ymax = max(y) + dy * 0.3
+    else:
+        ymax = max(y) + dy * 0.1
+    ax.set_ylim(ymin, ymax)
+
+    ## Show standard deviation
+    if mean and std:
+        _plot_curve_std(x, y, mean, std)
+
+    return fig
+
+
+def plot_curve(drv, method, mean=False, std=False, **kwargs):
+    """ Plot a curve which contains the data returned by *method* which
+    correspond to *drv*. If *mean*, annotate the mean. If *std*, in addition,
+    annotate the standard deviation.
+
+    This function accepts any plot-related kwargs.
+    """
+    _plot_graph(_plot_curve, drv, method, mean=mean, std=std, **kwargs)
+
+
+#####################################
+## ----- Bars Plot Functions ----- ##
+#####################################
+
+def _plot_bars(x, y, mask=None, **kwargs):
+    ## Set figure and axes
+    fig, ax = _plot_axes(**kwargs)
 
     ## Set x limits
     ax.set_xlim(min(x), max(x))
@@ -167,13 +186,12 @@ def _plot_bars(x, y, xlabel=None, ylabel=None, mask=None, title=None,
     return fig
 
 
-def plot_pmf_bars(drv, filename=None, figsize=None, dpi=None, xkcd=False):
-    """ Plot the probability mass function of the random variable *drv* as a
-    bar plot; if *filename* is given, save the figure, otherwise show it. If
-    *xkcd*, plot it in ``xkcd`` style. """
-    plotter = _plot_bars
-    if xkcd:
-        plotter = fn.partial(_plot_xkcd, plotter)
-    plot_kwargs = dict(figsize=figsize, dpi=dpi)
-    _plot_pmf(plotter, drv, filename=filename, plot_kwargs=plot_kwargs)
+def plot_bars(drv, method, **kwargs):
+    """ Plot a bar plot which contains the data returned by *method* which
+    correspond to *drv*. If *mean*, annotate the mean. If *std*, in addition,
+    annotate the standard deviation.
+
+    This function accepts any plot-related kwargs.
+    """
+    _plot_graph(_plot_bars, drv, method, **kwargs)
 
