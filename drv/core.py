d786c9a9a0be7040729a5035274deea9f7ddbf1d
Peleg Michaeli <peleg@palgo-at.com>
2014-10-12 16:43:11 +0300

    Implementing probability spaces
    Replacing core module.

--- drv/core.py
+++ drv/core.py
@@ -1,32 +1,27 @@
 """
-.. drv.py
+.. core.py
+
+Discrete random variables - core module.
 """
 
-## Abstract layer
-import abc
-
-## General tools
-import collections as col
-import itertools as it
-import operator as op
-import numpy as np
-import drv.tools as tools
-
-
-## Messages
-LIST_LENGTHS = "Value list and probability list must have the same lengths."
-NEG_PROB = "Cannot instantiate DRV with negative probability."
+import pspace
 
 
 class DRV(object):
     """ A general Discrete Random Variable. Treated as categorical. May have
     finite or infinite support.
 
-    *name* is an identifier for the random variable. """
-    __metaclass__ = abc.ABCMeta
+    A discrete random variable is simply a probability space equipped with a
+    function from that probability space into some outcome set, which is
+    usually, but not always, the set of reals.
 
-    def __init__(self, name):
+    A DRV comes with a *name*, which identifies the variable. """
+    def __init__(self, pspace, func, name):
+        self.pspace = pspace
+        self.func = func
         self.name = name
+
+    ## ----- Representation ----- ##
 
     @property
     def _cls(self):
@@ -39,37 +34,8 @@ class DRV(object):
     def __str__(self):
         return self.name
 
-    ## ----- Roll Methods ----- ##
-
-    def __call__(self):
-        return self.roll()
-
-    def _roll(self):
-        """ Return a result of a single roll. """
-        ## The 1 - x is in order to generate a number in (0,1]
-        return self.ppf(1 - np.random.random())
-
-    def roll(self, n=None):
-        """ Roll *n* times, if *n* is given, or else a single time; return the
-        results as a list, if *n* is given, or as a single value otherwise. """
-        if n is None:
-            return self._roll()
-
-        return list(self.rolls_gen(n=n))
-
-    def rolls_gen(self, n=None):
-        """ Return a generator of rolls. If *n* is given, limit the number of
-        rolls by *n*. """
-        if n is None:
-            n = inf
-        c = 0
-        while c < n:
-            c += 1
-            yield self._roll()
-
     ## ----- Probability Properties ----- ##
 
-    @abc.abstractproperty
     def mode(self):
         """ The value at which the probability mass function takes its maximum
         value.
@@ -78,68 +44,52 @@ class DRV(object):
         exists, this is an arbitrary mode. """
         raise NotImplementedError
 
-    @abc.abstractproperty
     def support(self):
         """ The values at which the probability mass function is positive. """
         raise NotImplementedError
 
     ## ----- Probability Methods ----- ##
 
-    @abc.abstractmethod
     def pmf(self, k):
         """ Return the probability mass function at *k*. """
         raise NotImplementedError
 
     ## ----- Operations ----- ##
 
-    @abc.abstractmethod
-    def map(self, function, name, klass=None):
-        """ Return a random variable whose values are the application of
-        *function* to self's values. """
+    def flatten(self):
+        """ Return a random variable with a new probability space whose sample
+        set is the inverse image of func. """
         raise NotImplementedError
 
+    def map(self, function, name, klass=None, flatten=False):
+        """ Return a random variable whose values are the application of
+        *function* to self's values. """
+        klass = klass or self.__class__
+        func = lambda x: function(self.func(x))
+        drv = klass(self.pspace, func, name)
+        if not flatten:
+            return drv
+        return drv.flatten()
 
-class FDRV(DRV):
+
+class FDRV(object):
     """ A finite (general) Discrete Random Variable. Treated as categorical.
 
     *name* is an identifier for the random variable. *xs* are the possible
     values (categories), and *ps* are the distributions. *xs* and *ps* are
     checked for correctedness, and *ps* are normalized. """
     def __init__(self, name, xs, ps):
+        self.pspace = pspace.FDPSpace(ps)
+        self.xs = xs
+        self._xdict = dict(enumerate(xs))
         self.name = name
-        self._initialize(xs, ps)
 
-    def _initialize(self, xs, ps):
-        """ Set and check *xs* and *ps*, ignoring values with zero probability,
-        normalizing the probabilities. """
-        ## Check for equal list lengths
-        if len(xs) != len(ps):
-            raise ValueError(LIST_LENGTHS)
-
-        ## Check for negative probabilities
-        if min(ps) < 0:
-            raise ValueError(NEG_PROB)
-
-        ## Remove zero probabilities
-        p_xs, p_ps = tools.unzip((x, p) for x, p in it.izip(xs, ps) if p > 0)
-
-        ## Aggregate equal values
-        d = col.defaultdict(float)
-        for x, p in zip(p_xs, p_ps):
-            d[x] += p
-        ag_xs, ag_ps = tools.unzip(d.iteritems())
-
-        ## Normalize probabilities
-        s_ps = sum(ag_ps)
-        n_ps = [p / s_ps for p in ag_ps]
-
-        self.xs, self.ps = ag_xs, n_ps
+    def func(self, k):
+        return self._xdict[k]
 
     @property
     def items(self):
-        return zip(self.xs, self.ps)
-
-    ## ----- Probability Properties ----- ##
+        return zip(self.xs, self.pspace.ps)
 
     @property
     def mode(self):
@@ -153,7 +103,7 @@ class FDRV(DRV):
     @property
     def support(self):
         """ The values at which the probability mass function is positive. """
-        return self.xs
+        return [x for x, p in self.items if p > 0]
 
     ## ----- Probability Methods ----- ##
 
@@ -175,7 +125,7 @@ class FDRV(DRV):
             i = self.xs.index(k)
         except ValueError:
             return 0.
-        return self.ps[i]
+        return self.pspace.ps[i]
 
     ## ----- Probability Inverse Methods ----- ##
 
@@ -214,162 +164,4 @@ class FDRV(DRV):
 
         ## We may need to be able to handle it
         raise NotImplementedError
-
-    def map(self, function, name, klass=None):
-        """ Return a random variable whose values are the application of
-        *function* to self's values. Assign *name* to the resulted random
-        variable, whose class is *klass* or self's class. """
-        f_name = name.format(self=self)
-        m_xs = [function(x) for x in self.xs]
-        klass = klass or self.__class__
-        return klass(name, m_xs, self.ps)
-
-
-class TDRV(DRV):
-    """ A Discrete Random Variable whose values are all tuples of a fixed
-    (finite) length. It is thought of as a tuple of (general) random variables.
-    """
-    def __init__(self, drvs, name=None):
-        self.drvs = drvs
-
-        if not name:
-            if len(self.drvs) == 1:
-                name = "({},)".format(self.drvs[0].name)
-            else:
-                name = "(" + ",".join(drv.name for drv in self.drvs) + ")"
-        self.name = name
-
-        self.formatter = dict(("_{i}".format(i=i), drv) for i, drv in
-                              enumerate(self.drvs))
-
-    ## ----- Data Model ----- ##
-
-    def __len__(self):
-        return len(self.drvs)
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            slice_attrs = []
-            slice_attrs.append(key.start)
-            slice_attrs.append(key.stop)
-            slice_attrs.append(key.step)
-            name = "{}[{}]".format(self.name, tools.slice_repr(key))
-            return self.subtuple(key, name)
-        return self.drvs[key]
-
-    ## ----- Probability Properties ----- ##
-
-    @property
-    def mode(self):
-        """ The value at which the probability mass function takes its maximum
-        value.
-
-        .. warning:: the mode is not necessarily unique. If more than one mode
-        exists, this is an arbitrary mode. """
-        return tuple(drv.mode for drv in self.drvs)
-
-    ## ----- Probability Methods ----- ##
-
-    def pmf(self, t):
-        """ Return the probability mass function at *t*. """
-        try:
-            if len(t) != len(self):
-                return 0.0
-        except TypeError:
-            return 0.0
-
-        pmfs = [drv.pmf(k) for drv, k in zip(self.drvs, t)]
-        return reduce(op.mul, pmfs, 1.0)
-
-    ## ----- Operations ----- ##
-
-    def subtuple(self, indices, name, klass=None):
-        """ Return the sub-tuple containing only *indices*. """
-        drvs = self.drvs[indices]
-        klass = klass or self.__class__
-        return klass(drvs, name=name)
-
-
-class TFDRV(TDRV):
-    """ A Discrete Random Variable whose values are all tuples of a fixed
-    (finite) length. It is thought of as a tuple of (general) finite random
-    variables.
-    """
-    @property
-    def xs(self):
-        return list(it.product(*(drv.xs for drv in self.drvs)))
-
-    @property
-    def ps(self):
-        return [self.pmf(x) for x in self.xs]
-
-    ## ----- Probability Properties ----- ##
-
-    @property
-    def support(self):
-        """ The values at which the probability mass function is positive. """
-        return list(it.product(drv.support for drv in self.drvs))
-
-    ## ----- Operations ----- ##
-
-    def bisect_op(self, operator):
-        n = len(self)
-
-        if n == 1:
-            return self[0]
-
-        mid = n - n / 2
-        a = self[:mid]
-        b = self[mid:]
-
-        return operator(a.bisect_op(operator), b.bisect_op(operator))
-
-    def bisect_reduce(self, function, name, klass=None, unpack=True):
-        """ Return a random variable whose values are the binary-reduced
-        application of *function* to self's values, applied binary. Assign
-        *name* to the resulted random variable, whose class is *klass* or
-        self's class.
-
-        This is sometimes a little bit faster than reduce. """
-        f_name = name.format(**self.formatter)
-        if unpack:
-            func = function
-        else:
-            func = lambda x, y: function((x, y))
-
-        red = self.bisect_op(func)
-
-        klass = klass or self.__class__
-        return klass(f_name, red.xs, red.ps)
-
-    def map(self, function, name, klass=None, unpack=False):
-        """ Return a random variable whose values are the application of
-        *function* to self's values. Assign *name* to the resulted random
-        variable, whose class is *klass* or self's class. """
-        f_name = name.format(**self.formatter)
-        xs = self.xs
-        ps = [self.pmf(x) for x in xs]
-        if unpack:
-            m_xs = [function(*x) for x in xs]
-        else:
-            m_xs = [function(x) for x in xs]
-        klass = klass or self.__class__
-        return klass(f_name, m_xs, ps)
-
-    def reduce(self, function, name, initial=None, klass=None, unpack=True):
-        """ Return a random variable whose values are the binary-reduced
-        application of *function* to self's values, applied linearly. Assign
-        *name* to the resulted random variable, whose class is *klass* or
-        self's class. """
-        f_name = name.format(**self.formatter)
-        if unpack:
-            func = function
-        else:
-            func = lambda x, y: function((x, y))
-        if initial:
-            red = reduce(func, self, initial)
-        else:
-            red = reduce(func, self)
-        klass = klass or self.__class__
-        return klass(f_name, red.xs, red.ps)
 
---
+++ drv/pspace.py
@@ -0,0 +1,138 @@
+"""
+.. pspace.py
+
+Probability spaces.
+"""
+
+import warnings
+import sympy
+
+
+## Messages
+INF_PROB = "Cannot instantiate PSpace with diverging probability."
+NEG_PROB = "Cannot instantiate PSpace with negative probability."
+NIE_PROB = "Could not determine if p is nonnegative."
+ZERO_PROB = "Cannot instantiate PSPace with zero-sum probabilities."
+
+
+## Tools
+def _normalize(p, precision, strict=False):
+    """ Return a normalized version of *p*, up to the given *precision*.
+
+    We use SymPy tools to verify and normalize p. """
+    ## This is an attempt to verify that p is always positive
+    ## In many cases, this does not work. If this is such a case, raise
+    ## ValueError if strict, or assume the minimum is 0 otherwise.
+    ##
+    ## See for example https://github.com/sympy/sympy/issues/8221 
+    k = sympy.Symbol('k', integer=True, nonnegative=True)
+    try:
+        nonneg = sympy.solve(p(k) >= 0)
+    except NotImplementedError:
+        if strict:
+            raise ValueError(NIE_PROB)
+        else:
+            nonneg = True
+    if not nonneg in (True,):
+        raise ValueError(NEG_PROB)
+
+    n = sympy.Symbol('n', integer=True, nonnegative=True)
+    sym_sum = sympy.Sum(p(n), (n, 0, sympy.oo)).doit()
+
+    ## The following does not always work, as there are some SymPy bugs
+    ## See for example https://github.com/sympy/sympy/issues/8219
+    ## In case it fails with an AttributeError, we warn but assume p is ok as
+    ## is.
+    try:
+        _sum = sym_sum.evalf(n=precision)
+        if _sum.is_infinite:
+            raise ValueError(INF_PROB)
+        f_sum = float(_sum)
+    except AttributeError:
+        warnings.warn("SymPy bug #8219; assumes p is normalized.")
+        return p
+
+    ## Don't allow 0 total probability
+    if not f_sum:
+        raise ValueError(ZERO_PROB)
+
+    def _p(k, p=p, f_sum=f_sum):
+        return p(k) / f_sum
+
+    return _p
+
+
+def _f_normalize(ps):
+    """ Return a normalized version of *ps*. """
+    ## Verify none of the ps is negative
+    if min(ps) < 0:
+        raise ValueError(NEG_PROB)
+
+    psum = sum(ps)
+    if not psum:
+        raise ValueError(ZERO_PROB)
+
+    return [1.0 * p / psum for p in ps]
+
+
+class DPSpace(object):
+    """ A general discrete probability space.
+
+    A probability space, in its most general settings, is a triple (Omega,F,P),
+    in which O is a sample space (any set), F is a set of events which is a
+    sigma-algebra (closed under complement and countable unions and
+    intersections), and P is a probability function which assigns a real number
+    from 0 to 1 to each event. The probability function P must return 0 for the
+    empty set, 1 for the whole set, and have the countable additivity property.
+
+    In practice, this module considers only discrete (countable) probability
+    spaces, so for this class the following assumptions are made:
+
+    1. The sample space Omega is the set of natural numbers (including 0)
+    2. The events set is the power set of Omega; however, we allow the
+       probability function P raise NotImplementedError for events for which
+       the probability calculation is difficult
+    3. The probability function P is described (by default) by the probability
+       function p which is a defined as p(x) = P({x}).
+
+    Thus, to create a :class:`DPSpace`, one only needs to provide the
+    probability function p. """
+    def __init__(self, p, precision=10):
+        """ Keep the probability function p, but normalize it first, using the
+        given *precision*. """
+        self.p = _normalize(p, precision=precision, strict=False)
+
+    def P(self, event):
+        """ Return the probability of *event*.
+
+        It is assumed that *event* is a finite iterable of naturals. """
+        ## TODO: implement infinite events
+        return sum(self.p(x) for x in event)
+
+
+class FDPSpace(DPSpace):
+    """ A finite (general) discrete probability space.
+
+    The following assumptions are made:
+
+    1. The sample space Omega is the set {0,...,n-1}, where n is the length of
+       the given iterable of probabilities
+    2. The events set is the power set of Omega
+    3. The probability function P is described (by default) by the probability
+       function p which is a defined as p(x) = P({x}).
+
+    Thus, to create a :class:`FDPSpace`, one only needs to provide the
+    probability function p. For practical purposes, that function is passed as
+    a finite iterable of values, which correspond to the probabilities of
+    Omega. """
+    def __init__(self, ps):
+        self.ps = _f_normalize(ps)
+
+    def p(self, k):
+        try:
+            return self.ps[k]
+        except IndexError:
+            return 0
+        except TypeError:
+            return 0
+
