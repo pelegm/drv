1566443be568e97bd084d3ff0d7848e624e53674
Peleg Michaeli <peleg@palgo-at.com>
2014-11-06 11:51:46 +0200

    Removing Omega_iter

--- drv/pspace.py
+++ drv/pspace.py
@@ -107,10 +107,6 @@ class DPSpace(object):
     def Omega(self):
         raise NotImplementedError
 
-    @property
-    def Omega_iter(self):
-        return iter(self.Omega)
-
     def p(self, k):
         raise NotImplementedError
 
