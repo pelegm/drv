2e95ea213f526bfc424f98f98eefbec83fb6184c
Peleg Michaeli <freepeleg@gmail.com>
2014-11-12 18:25:52 +0200

    Testing categorial pmf and cdf (re #17)

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -11,11 +11,15 @@ import pytest
 import drv.pspace
 import drv.rv
 
+## Symbolic
+import sympy
+
 
 ## Set data sets
 cfunc2 = lambda n: 1 / (n + 1) ** 2
 fdata = [range(1, 6), range(2, 5), [1, 1, 2, 3, 5, 8, 13, 21, 34, 55],
          [1, 1, 2, 0, 5, 8, 13, 0, 34, 0]]
+cat_data = [None, (1,), False, frozenset([2, 3]), int]
 
 
 ## Set probability spaces
@@ -30,14 +34,35 @@ pdps = [drv.pspace.ProductDPSpace(*pss) for pss in
 ## Set random variables
 I = lambda x: x
 f = lambda x: (x % 5) ** 2
+cat_f = lambda w: cat_data[w]
 drv_ = drv.rv.DRV('drv', dps, I)
 fdrv = [drv.rv.FDRV('fdrv{}'.format(n), ps, I) for n, ps in enumerate(fdps)]
 fdrv_f = [drv.rv.FDRV('fdrv{}'.format(n), ps, f) for n, ps in enumerate(fdps)]
+cat_rv = drv.rv.FDRV('cat_rv', fdps[0], cat_f)
 
 
 ################################################
 ## ----- Testing Probability Properties ----- ##
 ################################################
+
+
+def test_categorial_pdf():
+    ## Finite, categorial
+    n = sum(fdata[0])
+    for i, cat in enumerate(cat_data):
+        assert cat_rv.pmf(cat) == sympy.Rational(i + 1, n)
+
+
+def test_categorial_cdf():
+    ## Finite, categorial
+    p = sympy.Rational(1, 5)
+    ## TODO: these still fail
+    ## This needs decision; see #17
+    #assert cat_rv.cdf(-1) == 0
+    #for i in xrange(6):
+        #    assert cat_rv.cdf(i) == p * (i + 1)
+    #assert cat_rv.cdf(6) == 1
+
 
 def test_mode():
     ## Infinite
